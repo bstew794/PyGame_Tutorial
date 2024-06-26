a8f7b2ed962c3062289cbf5b0d46939d1a9fd5e7
Braeden Stewart <bstewart794@gmail.com>
2024-06-25 23:24:06 -0600

    Updated player and enemies to use rectangle for movement and display

--- Runner.py
+++ Runner.py
@@ -6,6 +6,9 @@ from sys import exit
 # initialize the pygame system to allow me to run and display the game
 pygame.init()
 
+# Set the name of the window
+pygame.display.set_caption('Runner')
+
 # define some variables that will be used in the rest of the code including the screen, clock, and fonts
 screen = pygame.display.set_mode((800, 400))
 clock = pygame.time.Clock()
@@ -18,14 +21,9 @@ text_surf = test_font.render('My game', False, 'Black')
 snal_surf = pygame.image.load('Graphics/Enemies/Snail/snail1.png').convert_alpha()
 play_surf = pygame.image.load('Graphics/Player/walk1.png').convert_alpha()
 
-# set snail x position and assign to a variable
-snal_x_pos = 872
-
-# define a rectangle hitbox for the player
+# define a rectangle hitbox for the player, and enemies
 play_rect = play_surf.get_rect(midbottom = (80, 300))
-
-# display an opening screen and change the fill of the test surface to red
-pygame.display.set_caption('Runner')
+snal_rect = snal_surf.get_rect(bottomleft = (800, 300))
 
 # main game loop
 while True:
@@ -36,11 +34,16 @@ while True:
             pygame.quit()
             exit()
     
-    # move the snail by a predetermined amount
-    snal_x_pos -= 4
 
-    if snal_x_pos < -72:
-        snal_x_pos = 872
+    # move the snail by a predetermined amount
+    snal_rect.left -= 4
+
+    if snal_rect.right <= 0:
+        snal_rect.left = 800
+
+    # move the player by a predetermined amount
+    play_rect.left += 1
+
 
     # display the skybox surface on the screen surface
     screen.blit(sky_surf, (0, 0))
@@ -52,10 +55,11 @@ while True:
     screen.blit(play_surf, play_rect)
 
     # display the snail on the screen surface
-    screen.blit(snal_surf, (snal_x_pos, 264))
+    screen.blit(snal_surf, snal_rect)
 
     # display the text surface on the screen surface
     screen.blit(text_surf, (300, 50))
+
 
     # update the visual aspects of the game for each loop
     pygame.display.update()
