0699b6c26cf39cb186eedbf7992700567f418e7d
Braeden Stewart <bstewart794@gmail.com>
2024-06-25 21:53:09 -0600

    Added a custom font and added a title surface

--- Runner.py
+++ Runner.py
@@ -6,16 +6,20 @@ from sys import exit
 # initialize the pygame system to allow me to run and display the game
 pygame.init()
 
-# define some variables that will be used in the rest of the code including images, the clock, and surfaces
+# define some variables that will be used in the rest of the code including the screen, clock, and fonts
 screen = pygame.display.set_mode((800, 400))
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('Font/PixelType.ttf', 50)
+
+#define some surfaces including the environment, actors, and text
 sky_surf = pygame.image.load('Graphics/Enviroment/Sky.png')
 grnd_surf = pygame.image.load('Graphics/Enviroment/Ground.png')
-test_surface = pygame.Surface((100, 200))
+test_surf = pygame.Surface((100, 200))
+text_surf = test_font.render('My game', False, 'Black')
 
 # display an opening screen and change the fill of the test surface to red
 pygame.display.set_caption('Runner')
-test_surface.fill('Red')
+test_surf.fill('Red')
 
 # main game loop
 while True:
@@ -32,8 +36,11 @@ while True:
     # display the ground surface on the screen surface
     screen.blit(grnd_surf, (0, 300))
 
-    # display the test surface on the skybox surface
-    sky_surf.blit(test_surface, (200, 100))
+    # display the text surface on the screen surface
+    screen.blit(text_surf, (300, 50))
+
+    # display the test surface on the screen surface
+    screen.blit(test_surf, (200, 100))
 
     # update the visual aspects of the game for each loop
     pygame.display.update()
